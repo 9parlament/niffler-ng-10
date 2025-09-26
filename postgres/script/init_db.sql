f8fb5fb54844fd46ce6ec564126527281fcc0898
9parlament <9parlament@gmail.com>
2025-09-26 11:58:06 +0700

    NG-10: Скорректирована реализация инита инфраструктуры для локального запуска

---
+++ postgres/script/init_db.sql
@@ -0,0 +1,6 @@
+-- init_db.sql
+-- Creates initial databases for each Niffler microservice
+CREATE DATABASE "niffler-auth";
+CREATE DATABASE "niffler-currency";
+CREATE DATABASE "niffler-spend";
+CREATE DATABASE "niffler-userdata";
