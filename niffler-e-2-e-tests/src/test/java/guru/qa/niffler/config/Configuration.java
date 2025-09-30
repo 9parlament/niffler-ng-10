bac3a41f2cd7ba18223765d58c3120e17999fc07
9parlament <9parlament@gmail.com>
2025-09-30 11:48:43 +0700

    NG-10: Скорректирована реализация управления конфигурацией в тестах

---
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/config/Configuration.java
@@ -0,0 +1,50 @@
+package guru.qa.niffler.config;
+
+import org.aeonbits.owner.Config;
+import org.aeonbits.owner.ConfigFactory;
+
+/**
+ * Управляет конфигурацией тестовых окружений.
+ * <p>
+ * Автоматически загружает настройки из файла конфигурации, соответствующего
+ * активному профилю. Профиль определяется через системную переменную {@code profile}.
+ * <p>
+ * <b>Профиль по умолчанию:</b> {@code local}
+ */
+@Config.Sources("classpath:config.${profile}.properties")
+public interface Configuration extends Config {
+    Configuration CFG = ConfigFactory.create(Configuration.class);
+
+    // Browser Configuration
+    @Key("ui.browser.browser")
+    String browser();
+
+    @Key("ui.browser.version")
+    String browserVersion();
+
+    @Key("ui.browser.remote")
+    boolean isRemoteBrowser();
+
+    @Key("ui.browser.remoteUrl")
+    String browserRemoteUrl();
+
+    // URL Configuration
+    @Key("url.front.base")
+    String frontBaseUrl();
+
+    @Key("url.front.auth")
+    String frontAuthUrl();
+
+    @Key("url.api.spend")
+    String apiSpendUrl();
+
+    // Database Configuration
+    @Key("db.username")
+    String dbUsername();
+
+    @Key("db.password")
+    String dbPassword();
+
+    @Key("db.jdbcUrl")
+    String dbJdbcUrl();
+}
