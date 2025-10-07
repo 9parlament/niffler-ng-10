2214e8a031992369b7beb9311040a7392d5f9c73
9parlament <9parlament@gmail.com>
2025-10-07 11:24:24 +0700

    NG-10: Extension по созданию дефолтного пользователя перенесён на автоинит

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/LoginTests.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/LoginTests.java
@@ -1,28 +1,26 @@
 package guru.qa.niffler.test;
 
-import guru.qa.niffler.jupiter.extension.DefaultUserCreationExtension;
 import guru.qa.niffler.ui.page.LoginPage;
 import guru.qa.niffler.ui.core.Browser;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 
 import static guru.qa.niffler.model.User.DEFAULT_USER;
 
-@ExtendWith(DefaultUserCreationExtension.class)
 @DisplayName("Авторизация пользователя")
 class LoginTests {
 
     @Test
     @DisplayName("Открытие страницы регистрации при нажатии кнопки \"Create New Account\"")
-    void signUpPageShouldBeOpenWhenCreateNewAccountIsClicked() {
+    void signUpPageShouldBeOpenWhenCreateNewAccountIsClickedTest() {
         Browser.open(LoginPage.class)
                 .goToSignUpPage()
                 .checkPageIsOpened();
     }
 
     @Test
-    void shouldSuccessLoginWhenValidDataSubmitted() {
+    @DisplayName("Вход в систему с валидными данными существующего пользователя")
+    void shouldSuccessLoginWhenValidDataSubmittedTest() {
         Browser.open(LoginPage.class)
                 .login(DEFAULT_USER.getUsername(), DEFAULT_USER.getPassword())
                 .checkIsOpened();
