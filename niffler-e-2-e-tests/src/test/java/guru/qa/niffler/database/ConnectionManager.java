467fca8b665f469a94ead9b39f7a7604d13765bc
9parlament <9parlament@gmail.com>
2025-10-24 22:29:33 +0700

    NG-10: Добавлена реализация ConnectionManager для создания соединения к БД. Получение соединения переведено на реализацию через DataSource

---
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/database/ConnectionManager.java
@@ -0,0 +1,57 @@
+package guru.qa.niffler.database;
+
+import com.atomikos.jdbc.AtomikosDataSourceBean;
+import lombok.NoArgsConstructor;
+import lombok.SneakyThrows;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+import static lombok.AccessLevel.PRIVATE;
+
+@NoArgsConstructor(access = PRIVATE)
+public class ConnectionManager {
+    private static final Map<Database, DataSource> DATASOURCE_STORE = new ConcurrentHashMap<>();
+    private static final Map<Long, Map<Database, Connection>> THREADED_CONN_POOL = new ConcurrentHashMap<>();
+
+    public static Connection getConnection(Database database) {
+        return THREADED_CONN_POOL.computeIfAbsent(
+                Thread.currentThread().threadId(),
+                key -> Map.of(database, createConnectionTo(database))
+        ).computeIfAbsent(
+                database,
+                key -> createConnectionTo(database)
+        );
+    }
+
+    public static void closeAllConnections() {
+        THREADED_CONN_POOL.values().stream()
+                .flatMap(db -> db.values().stream())
+                .forEach(ConnectionManager::closeSilently);
+    }
+
+    @SneakyThrows
+    private static Connection createConnectionTo(Database database) {
+        DataSource dataSource = DATASOURCE_STORE.computeIfAbsent(
+                database,
+                db -> {
+                    AtomikosDataSourceBean dsBean = new AtomikosDataSourceBean();
+                    dsBean.setUniqueResourceName(database.getDbName());
+                    dsBean.setXaDataSourceClassName("org.postgresql.xa.PGXADataSource");
+                    dsBean.getXaProperties().put("URL", database.connectionUrl());
+                    return dsBean;
+                }
+        );
+        return dataSource.getConnection();
+    }
+
+    private static void closeSilently(Connection connection) {
+        try {
+            connection.close();
+        } catch (SQLException e) {
+        }
+    }
+}
